a9e7bed3b99746908be8af902acd161de4fcba76
alskjd33 <alskjd33@gmail.com>
2024-07-01 13:01:47 +0900

    top 버튼 적용 애니메이션 효과/ 호버로 말표시나타내기 애니메이션으로 이미지 움직이기 /* 애미메이션 효과 */ a 태그의#은 젤 위 페이지로 이동 화살표 이미지도 아래,위 방향으로 전환 /* 화살표 방향 바꾸기 */
    자바스트립트 이용해서 페이지 아래위로 움직임

--- js/main.js
+++ js/main.js
@@ -15,4 +15,32 @@ window.onload = function () {
         section.scrollIntoView({ behavior: "smooth" });
     }
   }
+  // TOP 버튼 스크롤 기능 (업,다운)=========================
+const topBtn = document.getElementById("top-btn")
+topBtn.addEventListener("click" , function (event)
+{event.preventDefault()
+  console.log(window.scrollY);
+if(window.scrollY == 0)
+  {window.scrollTo({
+top:2496,
+behavior: "smooth"
+  });
+}
+else{
+  window.scrollTo({
+    top:0,
+    behavior: "smooth"
+})}
+})
+// 회살표 이미지 회전
+const topBtnImg = document. getElementById("top-btn-img")
+window.addEventListener("scroll", function(sctop){
+scTop = window.document. documentElement. scrollTop;
+if(scTop > 0){
+  topBtnImg.classList.add("up")
+} 
+else{
+  topBtnImg.classList.remove("up")
+}
+})
 };
