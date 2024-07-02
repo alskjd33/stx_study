3641cf5e321350543fe09b7aa37e98db5104c044
alskjd33 <alskjd33@gmail.com>
2024-07-02 11:16:57 +0900

    모달창 만들기  position: fixed; 주는 방법 (부모한테주기) 자바스크립으로 창닫기, 스크롤 기능 멈추고 움직이는것

--- js/main.js
+++ js/main.js
@@ -43,4 +43,22 @@ else{
   topBtnImg.classList.remove("up")
 }
 })
+// 모달창(안내창)============================
+const body = document.querySelector("body")
+const modal = document.querySelector(".modal-wrap")
+const modalClose = document.querySelector(".madal-close")
+// isOpen 값에 따라 스크롤을 제어하는 함수
+function controlScroll(isOpen){
+  if(isOpen){
+body.style.overflow = "hidden";
+  }else{body.style.overflow = "auto";}
+}
+// 초기 모달창 상태 설정
+const isOpen = true;
+controlScroll(isOpen)
+modalClose.addEventListener("click",function(){
+  modal.style.display = "none"
+  // 모달창이 닫히면 스크롤이 활성화됨
+  controlScroll(false)
+})
 };
